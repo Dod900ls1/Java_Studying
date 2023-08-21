c7554ab08b685b9b9e4c3b7b92c4c88211d7cc8d
Yehor Boiar <boyaregor1@gmail.com>
2023-08-21 10:02:00 +0100

    init

---
+++ OOP/OOPHierarchies/Lab1/Cargo.java
@@ -0,0 +1,17 @@
+package Lab1;
+
+public class Cargo extends Car {
+
+    @Override
+    public int getSpeed() {
+        // TODO Auto-generated method stub
+        return super.getSpeed();
+    }
+
+    @Override
+    public void setSpeed(int speed) {
+        // TODO Auto-generated method stub
+        super.setSpeed(speed);
+    }
+    
+}
